85807f5b9bd0b703a863e7206b27f54c73c9e76a
Dmitry Yakorev <urakend@mail.ru>
2024-03-14 15:11:20 +0300

    feat:new5_up1

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -136,8 +136,18 @@ function isDateInPeriod(date, period) {
  * '1999-01-05T02:20:00.000Z' => '1/5/1999, 2:20:00 AM'
  * '2010-12-15T22:59:00.000Z' => '12/15/2010, 10:59:00 PM'
  */
-function formatDate(/* date */) {
-  throw new Error('Not implemented');
+function formatDate(date) {
+  const res = new Intl.DateTimeFormat('en', {
+    year: 'numeric',
+    month: 'numeric',
+    day: 'numeric',
+    hour: 'numeric',
+    minute: '2-digit',
+    second: '2-digit',
+    hour12: 'true',
+    timeZone: 'UTC',
+  });
+  return res.format(new Date(date));
 }
 
 /**
@@ -152,8 +162,14 @@ function formatDate(/* date */) {
  * 12, 2023 => 10
  * 1, 2024 => 8
  */
-function getCountWeekendsInMonth(/* month, year */) {
-  throw new Error('Not implemented');
+function getCountWeekendsInMonth(month, year) {
+  const day = new Date(year, month - 1);
+  let count = 0;
+  while (day.getMonth() === month - 1) {
+    if (day.getDay() === 0 || day.getDay() === 6) count += 1;
+    day.setDate(day.getDate() + 1);
+  }
+  return count;
 }
 
 /**
@@ -169,8 +185,14 @@ function getCountWeekendsInMonth(/* month, year */) {
  * Date(2024, 0, 31) => 5
  * Date(2024, 1, 23) => 8
  */
-function getWeekNumberByDate(/* date */) {
-  throw new Error('Not implemented');
+function getWeekNumberByDate(date) {
+  const temp = new Date(date.getFullYear(), 0);
+  let count = 1;
+  while (temp < date) {
+    if (temp.getDay() === 0) count += 1;
+    temp.setDate(temp.getDate() + 1);
+  }
+  return count;
 }
 
 /**
