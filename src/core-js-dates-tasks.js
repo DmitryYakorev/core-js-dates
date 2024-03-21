ee5293a1e64146f924544cfa5657f89ddaf64bc0
Dmitry Yakorev <urakend@mail.ru>
2024-03-21 16:23:21 +0300

    feat:final1_up1

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -279,8 +279,11 @@ function getWorkSchedule(period, countWorkDays, countOffDays) {
  * Date(2022, 2, 1) => false
  * Date(2020, 2, 1) => true
  */
-function isLeapYear(/* date */) {
-  throw new Error('Not implemented');
+function isLeapYear(date) {
+  if (date.getFullYear() % 4 !== 0) return false;
+  if (date.getFullYear() % 100 !== 0) return true;
+  if (date.getFullYear() % 400 !== 0) return false;
+  return true;
 }
 
 module.exports = {
